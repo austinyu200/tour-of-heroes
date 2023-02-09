ace62376e251546782243d0a4eded521f53ecb0b
Austin <austin@mail.icash.com.tw>
2023-02-09 17:10:42 +0800

    display-a-selection-list

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit{
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+    skill: 'storm'
+  };  
+  heros = HEROES;
+  selectedHero?: Hero;
+  constructor() { }
+  
+  ngOnInit(): void {      
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice', skill: 'unknow' },
+    { id: 13, name: 'Bombasto', skill: 'unknow' },
+    { id: 14, name: 'Celeritas', skill: 'unknow' },
+    { id: 15, name: 'Magneta', skill: 'unknow' },
+    { id: 16, name: 'RubberMan', skill: 'unknow' },
+    { id: 17, name: 'Dynama', skill: 'unknow' },
+    { id: 18, name: 'Dr. IQ', skill: 'unknow' },
+    { id: 19, name: 'Magma', skill: 'unknow' },
+    { id: 20, name: 'Tornado', skill: 'unknow' }
+];
